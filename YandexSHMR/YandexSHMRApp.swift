c5e475c2312695f419b189cd45556a46895273f5
Igmnl <senior.nikitos.a@gmail.com>
2025-06-14 02:07:32 +0300

    Initial Commit

---
+++ YandexSHMR/YandexSHMRApp.swift
@@ -0,0 +1,17 @@
+//
+//  YandexSHMRApp.swift
+//  YandexSHMR
+//
+//  Created by Никита Арабчик on 14.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct YandexSHMRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
